882422b7cf001b67d85d192d3deb010264b5f11c
Roy Hyunjin Han <rhh@crosscompute.com>
2020-10-10 10:13:21 -0400

    Remove old reference

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,3 @@
-from os import remove
 from os.path import curdir, join
 from shutil import rmtree
 
@@ -7,8 +6,6 @@ PACKAGE_FOLDER = join(curdir, '{{ cookiecutter.package_name }}')
 
 
 if __name__ == '__main__':
-    if '{{ cookiecutter.security_package }}' == 'none':
-        remove(join(PACKAGE_FOLDER, 'models', 'user.py'))
     if '{{ cookiecutter.database_package }}' == 'none':
         rmtree(join(PACKAGE_FOLDER, 'migrations'))
         rmtree(join(PACKAGE_FOLDER, 'models'))
