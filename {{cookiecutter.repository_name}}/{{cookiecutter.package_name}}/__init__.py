410cbd95b9876fbc9e2febc8089dfbf71ad67082
Roy Hyunjin Han <rhh@crosscompute.com>
2020-10-20 23:49:54 -0400

    Import models properly

--- {{cookiecutter.repository_name}}/{{cookiecutter.package_name}}/__init__.py
+++ {{cookiecutter.repository_name}}/{{cookiecutter.package_name}}/__init__.py
@@ -1,5 +1,9 @@
 from pyramid.config import Configurator
 
+{% if cookiecutter.database_package == 'records' -%}
+from . import models as M  # noqa
+{%- endif %}
+
 
 def main(global_config, **settings):
     with Configurator(settings=settings) as config:
@@ -14,6 +18,18 @@ def includeme(config):
     {% endif -%}
     {% if cookiecutter.security_package == 'users' -%}
     config.include('invisibleroads_users')
+    configure_models(config)
     {% endif -%}
+    configure_views(config)
+
+
+{% if cookiecutter.security_package == 'users' -%}
+def configure_models(config):
+    from invisibleroads_users.models import User
+    M.User = User
+{%- endif %}
+
+
+def configure_views(config):
     config.include('.routes')
     config.scan('.views')
